c44d7f9aa0d7602e43d836cb5da8a9ea2305aa33
Yuning <37944809+Starscream29@users.noreply.github.com>
2020-01-02 13:34:04 -0700

    getting location in background thread

--- app/src/main/java/com/example/runtracker/MainActivity.java
+++ app/src/main/java/com/example/runtracker/MainActivity.java
@@ -2,19 +2,87 @@ package com.example.runtracker;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
+import android.os.AsyncTask;
 import android.os.Bundle;
+import android.util.Log;
+import android.util.Pair;
 import android.view.View;
 import android.widget.Button;
+
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
     Button startButton;
     Button stopButton;
+    LocationManager locationManager;
+    List<Pair<String,String>> gpsPoints;
+
+    private LocationListener mListener = new LocationListener() {
+        @Override
+        public void onLocationChanged(Location location) {
+            // Previously mock location is cleared.
+            // getLastKnownLocation(LocationManager.GPS_PROVIDER); will not return mock location.
+        }
+
+        @Override
+        public void onStatusChanged(String provider, int status, Bundle extras) {
+        }
+
+        @Override
+        public void onProviderEnabled(String provider) {
+        }
+
+        @Override
+        public void onProviderDisabled(String provider) {
+        }
+    };
+
 
 
     public void startTracking(View view) {
         switchButton();
+        AsyncTask.execute(new Runnable() {
+            @Override
+            public void run() {
+                Track();
+            }
+        });
+
+
     }
+
+    public void Track(){
+        while (true) {
+
+            locationManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
+            Location location;
+
+            locationManager.requestLocationUpdates(locationManager.GPS_PROVIDER, 2000, 10, mListener);
+            location = locationManager.getLastKnownLocation(LocationManager.GPS_PROVIDER);
+
+
+            String Long = location.getLongitude() + "";
+            String Lat = location.getLatitude() + "";
+            Pair<String, String> locationPoint = new Pair(Lat, Long);
+
+            try {
+                Thread.sleep(1000);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+
+        gpsPoints.add(locationPoint);
+
+
+            Log.v("succ", "Succccc");
+        }
+    }
+
 
     public void stopTracking(View view) {
         switchButton();
@@ -42,6 +110,7 @@ public class MainActivity extends AppCompatActivity {
 
         startButton = findViewById(R.id.start_button);
         stopButton = findViewById(R.id.stop_button);
+
 
     }
 
